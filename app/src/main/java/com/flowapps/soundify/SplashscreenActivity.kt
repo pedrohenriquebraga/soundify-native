7c215847b3ab7900467307b694836daedea282ca
pedrohenriquebraga <gamesantos7@gmail.com>
2025-02-07 16:29:56 -0300

    add splashscreen

---
+++ app/src/main/java/com/flowapps/soundify/SplashscreenActivity.kt
@@ -0,0 +1,36 @@
+package com.flowapps.soundify
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.flowapps.soundify.databinding.ActivitySplashscreenBinding
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashscreenBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
